2c32156cd636e82db63c2a68ce6705a06a86b50a
DavidPerez97 <38087886+DavidPerez97@users.noreply.github.com>
2019-02-20 12:48:15 -0800

    Finished pong

--- AgentPingBlock.js
+++ AgentPingBlock.js
@@ -10,44 +10,24 @@ function AgentPingBlock(game, spritesheet) {
     this.height = 100;
     this.width = 15;
     this.ctx = game.ctx;
-    this.boundBarrierLeft = 504;
-    this.boundBarrierRight = 964;
     this.boundingbox = new BoundingBox(this.x, this.y, this.width, this.height);
+    this.pongBall = this.game.pongBall;
+    this.increment = 5;
 }
 
 
 AgentPingBlock.prototype.update = function () {
+    //console.log(this.pongBall);
+    //console.log(this.pongBall.y);
+    //console.log(this);
+
     this.boundingbox.update(this.x, this.y);
-    if (this.game.key) {
-        if (this.game.key === 38) {
-            //console.log("up");
-            if(this.y > 0) {
-                this.y -= 10;
-            }
-        } else if (this.game.key === 40) {
-            //console.log("down");
-            if (this.y < 720 - this.height) {
-                this.y += 10;
-            }
-        } /*else if (this.game.key === 39) {
-            //console.log("right");
-            if (this.x < this.boundBarrierRight - this.width) {
-                this.x += 10;
-                if (this.x > this.boundBarrierRight -this.width) {
-                    this.x = this.boundBarrierRight - this.width;
-                }
-            }
-            
-        } else if (this.game.key === 37) {
-            //console.log("left");
-            if (this.x > this.boundBarrierLeft) {
-                this.x -= 10;
-                if (this.x < this.boundBarrierLeft) {
-                    this.x = this.boundBarrierLeft;
-                }
-            }
-        }*/
-        //this.game.key = false;
+
+    if ((this.pongBall.y + (this.pongBall.height / 2)) > this.y + (this.height / 2)) {
+        this.y += this.increment;
+
+    } else if ((this.pongBall.y + (this.pongBall.height / 2)) < this.y + (this.height / 2)) {
+        this.y -= this.increment;
     }
 
 }
--- Main.js
+++ Main.js
@@ -20,16 +20,19 @@ AM.downloadAll(function () {
     gameEngine.start();
 
     gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/PongBG.png")));
-
+    var pingPongBall = new PingBall(gameEngine, AM.getAsset("./img/pingBallOnYourSide.png"),
+                                                AM.getAsset("./img/pingBallOnEnemySide.png"));
+    gameEngine.pongBall = pingPongBall;
+    
     var playerPongBoard = new PlayerPingBlock(gameEngine, AM.getAsset("./img/PlayerPing.png")); //agent stuffAutomata(gameEngine);
     var playerPongBoard2 = new AgentPingBlock(gameEngine, AM.getAsset("./img/AgentPing.png")); //agent stuffAutomata(gameEngine);
     var pong_boxes = [];
     pong_boxes.push(playerPongBoard);
     pong_boxes.push(playerPongBoard2);
     gameEngine.pong_boxes = pong_boxes;
-    var pingPongBall = new PingBall(gameEngine, AM.getAsset("./img/pingBallOnYourSide.png"),
-                                                AM.getAsset("./img/pingBallOnEnemySide.png"));
     
+
+
 
     gameEngine.addEntity(playerPongBoard);
     gameEngine.addEntity(playerPongBoard2);
