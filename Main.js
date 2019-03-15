4a9373b30b08241f10b4c837292e5d14ff5def40
DavidPerez97 <38087886+DavidPerez97@users.noreply.github.com>
2019-03-14 18:57:45 -0700

    ???

--- Main.js
+++ Main.js
@@ -13,7 +13,7 @@ AM.queueDownload("./img/pingBallOnEnemySide.png");
 
 
 window.onload = function () {
-    var socket = io.connect("http://localhost:8888");   s 
+    var socket = io.connect("http://24.16.255.56:8888");
     socket.on("load", function (data) {
         console.log(data);
     });
